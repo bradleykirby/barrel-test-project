7dc7fe0c5c2bba18aaaa0b1e67f39c6257a4f862
Bradley Kirby <bradley@wallaceinline.com>
2020-03-17 07:30:28 -0400

    output content for hero

--- wp-content/themes/barrel-wordpress-test/modules/hero/hero.php
+++ wp-content/themes/barrel-wordpress-test/modules/hero/hero.php
@@ -1,11 +1,42 @@
 <section class="hero" data-module="hero">
+<?php
+    global $post;
+    $post = get_post($postID);
+    setup_postdata( $post );
+
+    $bg_image_id = get_post_thumbnail_id( get_the_ID() );
+    $bg_image = get_module('image', array(
+      'id' => $bg_image_id,
+      'cover' => false,
+      'contain' => false,
+      'absolute' => false,
+      'top' => true,
+      'size' => 'large'
+    ));
+
+    $fg_image_id = get_field('index__product')['ID'];
+    $fg_image = get_module('image', array(
+      'id' => $fg_image_id,
+      'cover' => false,
+      'contain' => false,
+      'absolute' => false,
+      'top' => false,
+      'size' => 'medium'
+    ));
+
+    $headline = get_field( "index__headline");
+    $content = get_the_content();
+?>
+
+<?php echo $bg_image; ?>
+
   <main>
 
-    <?php
-    $image = get_the_post_thumbnail_url( $post->ID, 'large' );
-    the_module('image', array(
-      'image' => $image
-    ));
-    ?>
+    <h1><?php echo $headline ?></h1>
+    <p><?php echo $content ?></p>
+    <?php echo $fg_image; ?>
+
+    
   </main>
+  <?php wp_reset_postdata(); ?>
 </section>
